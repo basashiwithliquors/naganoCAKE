921281bfa31659de21afe15388f19c7565b3a70c
EC2 Default User <mhyrso27@gmail.com>
2022-10-19 10:33:18 +0000

    [Add] public-mypage-editpage

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -9,6 +9,9 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
+   @customer = current_customer
+   @customer.update(customer_params)
+   redirect_to customers_mypage_path
   end
 
   def unsubscribe
