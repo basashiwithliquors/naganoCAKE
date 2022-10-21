047bce83d56eee86bc80af4ace5e9a7b6ff56674
SakiOjima <100123445+Jiima33@users.noreply.github.com>
2022-10-21 06:04:22 +0000

    [fix] git commit -m[fix]
    git commit -m[fix]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -8,7 +8,7 @@ class Public::ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
     if @item.is_active == false
-      redirect_to public_items_path
+      redirect_to items_path
     end
   end
  
