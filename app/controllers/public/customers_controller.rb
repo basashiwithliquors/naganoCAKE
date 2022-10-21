9be1da504a0544b928991202e6d7935a127b9439
EC2 Default User <mhyrso27@gmail.com>
2022-10-21 05:24:38 +0000

    fix-path

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -13,7 +13,7 @@ before_action :authenticate_customer!
   def update
    @customer = current_customer
    @customer.update(customer_params)
-   redirect_to public_customers_mypage_path
+   redirect_to customers_mypage_path
   end
 
   def unsubscribe
