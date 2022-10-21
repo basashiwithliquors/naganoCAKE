bc59af1e9f6e3a3bc276f6c292cb7602429b03df
EC2 Default User <mhyrso27@gmail.com>
2022-10-21 07:13:00 +0000

    admin-customer

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 # 顧客用
 # URL /customers/sign_in ...
-devise_for :customers, skip: [:passwords], controllers: {
+devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
@@ -13,23 +13,21 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
+# root to: "public/homes#top"
+# top
 root 'public/homes#top'
+# about
+# get 'about' => 'public/homes#about'
+
 
 namespace :admin do
-  root 'homes#top'
   resources :orders, only: [:show, :update] do
     resources :order_items, only: [:update]
   end
   resources :genres, only: [:index, :create, :edit, :update]
   resources :items, except: [:destroy]
-  resources :customers, only: [:index, :show, :edit, :update]
 end
 
-namespace :public do
-  get 'about' => 'homes#about', as: 'about'
-  resources :shipping_addresses, only: [:index, :edit, :create, :update, :destroy]
-  resources :cart_items, only:[:index, :update, :destroy, :create]
-=======
 scope module: :public do
   get 'about' => "homes#about", as: 'about'
 
@@ -40,14 +38,13 @@ scope module: :public do
   end
 
 
->>>>>>> origin/develop
   resources :items, only: [:index, :show]
   get 'orders/complete', to: 'orders#complete', as: 'complete_order'
   resources :orders, only: [:new, :index, :create, :show]
   post 'orders/confirm(/:id)', to: 'orders#confirm', as: 'confirm_order'
 
 
-  resource :customers, only: [:show, :edit, :update]
+  # resource :customers, only: [:show, :edit, :update]
   get "customers/mypage" => "customers#show"
   get "customers/information/edit" => "customers#edit"
   patch "customers/information" => "customers#update"
